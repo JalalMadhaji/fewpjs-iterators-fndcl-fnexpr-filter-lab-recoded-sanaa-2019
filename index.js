31c6f2f85c762fe3fa1ae6076233c460396a8e42
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-10-14 21:48:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+let jalals = function findMatching(drivers, qoute) {
+    return drivers.filter(data => data.toLowerCase() === qoute.toLowerCase());
+};
+let jas = function fuzzyMatch(drivers, qoute) {
+    return drivers.filter(data => data.startsWith(qoute));
+};
+let name = function matchName(drivers, qoute) {
+    return drivers.filter(data => data.name === qoute);
+};
